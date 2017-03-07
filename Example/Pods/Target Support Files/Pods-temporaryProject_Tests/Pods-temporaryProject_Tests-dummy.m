af04bae3a373cd33362b37ee90b7cc2d1150862c
Purushottam Chandra <purushottam.chandra@kuliza.com>
2017-03-07 08:11:33 +0530

    New

---
+++ Example/Pods/Target Support Files/Pods-temporaryProject_Tests/Pods-temporaryProject_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_temporaryProject_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_temporaryProject_Tests
+@end
---
+++ Example/Pods/Target Support Files/temporaryProject/temporaryProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_temporaryProject : NSObject
+@end
+@implementation PodsDummy_temporaryProject
+@end
